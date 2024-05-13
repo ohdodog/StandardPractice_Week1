a14b08af8297f63902add36dfa96f58ef8fa4788
ohdodog <97august4@naver.com>
2024-05-13 10:31:36 +0900

    캐릭터 이동구현

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RocketController : MonoBehaviour
+{
+    private EnergySystem _energySystem;
+    private RocketMovement _rocketMovement;
+    
+    private bool _isMoving;
+    private Vector2 _movementDirection;
+    
+    private void Awake()
+    {
+        _energySystem = GetComponent<EnergySystem>();
+        _rocketMovement = GetComponent<RocketMovement>();
+    }
+
+    // TODO : OnMove 구현
+    // private void OnMove...
+    private void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        _rocketMovement.ApplyMovement(moveInput);
+    }
+
+
+    // TODO : OnBoost 구현
+    // private void OnBoost...
+    private void OnBoost(InputValue value)
+    {
+        _rocketMovement.ApplyBoost(value.isPressed);
+    }
+}
---
+++ Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RocketMovement : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+    private bool _isBoosted;
+    private readonly float SPEED = 5f;
+    private readonly float ROTATIONSPEED = 0.01f;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(movementDirection);
+    }
+
+    public void ApplyMovement(Vector2 direction)
+    {
+        // TODO : 회전을 적용하고 이동을 적용함 -> 이에 대한 구현을 아래에서 진행할 것
+        // Rotate(direction);
+        // Move();
+        movementDirection = direction;
+    }
+
+    public void ApplyBoost(bool isPressed)
+    {
+        _isBoosted = isPressed;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        // TODO : 완만한 회전을 적용함
+        
+    }
+
+    private void Move(Vector2 direction)
+    {
+        // TODO : 움직임 적용
+        direction = direction * SPEED;
+        _rb2d.velocity = direction;
+
+
+    }
+}
